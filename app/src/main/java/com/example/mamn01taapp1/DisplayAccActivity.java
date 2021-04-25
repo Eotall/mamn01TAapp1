420229f0f394cd9cea88fe1b859907756b0897c2
eotall <teodor.ahlinder@me.com>
2021-04-25 18:55:00 +0200

    Version 2.0, added several functions but no sound!

--- app/src/main/java/com/example/mamn01taapp1/DisplayAccActivity.java
+++ app/src/main/java/com/example/mamn01taapp1/DisplayAccActivity.java
@@ -1,42 +1,51 @@
 package com.example.mamn01taapp1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DisplayAccActivity extends AppCompatActivity implements SensorEventListener {
 
-    private double x, y, z;
-    private TextView xText, yText, zText;
-
+    private TextView xText, yText, zText, orientationText;
+    private View bg;
+    private ImageView star;
     private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
+    private int red = 127;
+    private int blue = 127;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_acc);
-        x = 0;
-        y = 0;
-        z = 0;
 
         xText = (TextView) findViewById(R.id.xText);
         yText = (TextView) findViewById(R.id.yText);
         zText = (TextView) findViewById(R.id.zText);
+
+        orientationText = (TextView) findViewById(R.id.orientationText);
+
+        bg = (View) findViewById(R.id.bg);
+        star = (ImageView) findViewById(R.id.star);
+
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
@@ -45,16 +54,60 @@ public class DisplayAccActivity extends AppCompatActivity implements SensorEvent
         mSensorManager.unregisterListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onSensorChanged(SensorEvent event) {
-        z = Math.round(event.values[0]);
-        x = Math.round(event.values[1]);
-        y = Math.round(event.values[2]);
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            xText.setText("Value X: " + String.format("%,.2f", event.values[0]));
+            yText.setText("Value Y: " + String.format("%,.2f", event.values[1]));
+            zText.setText("Value Z: " + String.format("%,.2f", event.values[2]));
 
-        xText.setText("Value X: " + x);
-        yText.setText("Value Y: " + y);
-        zText.setText("Value Z: " + z);
 
+        } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            float x = event.values[0];
+            float y = event.values[1];
+
+            StringBuilder sb = new StringBuilder();
+            if (x > 0.2) {
+                sb.append(" Left");
+            } else if (x < -0.2) {
+                sb.append(" Right");
+            }
+            if (y > 0.2) {
+                sb.append(" Back");
+            } else if (y < -0.2) {
+                sb.append(" Forward");
+            }
+            orientationText.setText(sb.toString());
+
+            //calculate new background color
+            double step = 255/9.81;
+            //red += (x);
+            //blue += (y);
+            red = 255 - Math.abs((int) Math.round(x*step));
+            blue = 255 - Math.abs((int) Math.round(y*step));
+            bg.setBackgroundColor(Color.argb(100,red,0,blue));
+
+            //move star
+            float newX = star.getX()-x;
+            float newY = star.getY()+y;
+            float maxWidth = (bg.getWidth() - star.getWidth());
+            float minHeight = bg.getY();
+            float maxHeight = (bg.getHeight() - star.getHeight() + minHeight);
+            if (newX < 0) {
+                newX = 0;
+            } else if (newX > maxWidth) {
+                newX = maxWidth;
+            }
+            if (newY < minHeight) {
+                newY = bg.getY();
+            } else if (newY > maxHeight) {
+                newY = maxHeight;
+            }
+            star.setX(newX);
+            star.setY(newY);
+        }
     }
 
     @Override
--- app/src/main/java/com/example/mamn01taapp1/DisplayCompActivity.java
+++ app/src/main/java/com/example/mamn01taapp1/DisplayCompActivity.java
@@ -1,12 +1,18 @@
 package com.example.mamn01taapp1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaActionSound;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
@@ -18,6 +24,9 @@ public class DisplayCompActivity extends AppCompatActivity implements SensorEven
     private float currentDegree = 0f;
     private SensorManager mSensorManager;
     public TextView headingText;
+    private Vibrator v;
+    protected float[] magSensorVals;
+    private static final float ALPHA = 0.25f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +36,15 @@ public class DisplayCompActivity extends AppCompatActivity implements SensorEven
         compass = (ImageView) findViewById(R.id.compassView);
         headingText = (TextView) findViewById(R.id.headingText);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Vibrator
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME);
     }
 
@@ -41,30 +54,77 @@ public class DisplayCompActivity extends AppCompatActivity implements SensorEven
         mSensorManager.unregisterListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onSensorChanged(SensorEvent event) {
-        float degree = Math.round(event.values[0] * 10000) / 10000;
-        headingText.setText("Heading: " + Float.toString(degree) + " degrees");
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            magSensorVals = lowPass(event.values.clone(), magSensorVals);
 
-        // change picture based on rotation
-        if (Math.abs(degree) <= 15f) {
-            compass.setImageResource(R.drawable.compass_icon_north);
-        } else {
-            compass.setImageResource(R.drawable.compass_icon);
+
+        if(magSensorVals != null) {
+            // extract rotation from sensor
+            float degree = event.values[0];
+
+            // normalise rotation to 0-360 degrees
+            while (degree >= 360) {
+                degree -= 360;
+            }
+            while (degree < 0) {
+                degree += 360;
+            }
+
+            // display rotation in textView, rounded because we don't need the precision
+            headingText.setText("Heading: " + Float.toString(Math.round(degree)) + " degrees");
+
+            // change picture based on rotation
+            // also, play sound (doesn't work, app crashes)
+            //MediaPlayer mp1 = MediaPlayer.create(this, R.raw.single_click);
+            //MediaPlayer mp2 = MediaPlayer.create(this, R.raw.double_click);
+            //MediaActionSound sound = new MediaActionSound();
+
+            if (Math.abs(degree) <= 15f || Math.abs(degree) >= 345f) {
+                compass.setImageResource(R.drawable.compass_icon_north);
+                //sound.play(MediaActionSound.START_VIDEO_RECORDING);
+            } else if (Math.abs(degree) <= 105f && Math.abs(degree) >= 75f) {
+                compass.setImageResource(R.drawable.compass_icon_east);
+                //sound.play(MediaActionSound.STOP_VIDEO_RECORDING);
+            } else if (Math.abs(degree) <= 195f && Math.abs(degree) >= 165f) {
+                compass.setImageResource(R.drawable.compass_icon_south);
+                //sound.play(MediaActionSound.STOP_VIDEO_RECORDING);
+            } else if (Math.abs(degree) <= 285f && Math.abs(degree) >= 255f) {
+                compass.setImageResource(R.drawable.compass_icon_west);
+                //sound.play(MediaActionSound.STOP_VIDEO_RECORDING);
+            } else {
+                compass.setImageResource(R.drawable.compass_icon);
+            }
+
+            // create a rotation animation (reverse turn degree degrees)
+            RotateAnimation ra = new RotateAnimation(
+                    currentDegree,
+                    -degree,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f);
+            ra.setDuration(210);
+            ra.setFillAfter(true);
+            compass.startAnimation(ra);
+            currentDegree = -degree;
         }
+        // vibrate if phone is at too steep an angle
+        } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            if (Math.abs(event.values[0]) > 3 || Math.abs(event.values[1]) > 3) {
+                v.vibrate(400);
+                System.out.println("Nu vibrerar det!");
+            }
+        }
+    }
 
-        // create a rotation animation (reverse turn degree degrees)
-        RotateAnimation ra = new RotateAnimation(
-                currentDegree,
-                -degree,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f);
-        ra.setDuration(210);
-        ra.setFillAfter(true);
-        compass.startAnimation(ra);
-        currentDegree = -degree;
-
+    protected float[] lowPass( float[] input, float[] output ) {
+        if ( output == null ) return input;
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
     }
 
     @Override
